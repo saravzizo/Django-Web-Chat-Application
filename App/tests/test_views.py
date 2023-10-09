fe810557724e3ab2a399cb133ddfc0b388a8cfe3
noordeen123 <noordeen.cse_c2019@crescent.education>
2023-10-09 17:12:01 +0530

    Added tests

--- App/tests/test_views.py
+++ App/tests/test_views.py
@@ -16,5 +16,29 @@ class HomeViewTestCase(TestCase):
         self.assertTemplateUsed(response, 'home.html')
         self.assertContains(response, 'Hi There Welcome Home')
 
+class RegisterViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('register')
+
+    def test_register_view(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'register.html')
+        self.assertContains(response, 'Register Page') 
+
+class LoginViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('login')
+
+    def test_login_view(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'login.html')
+        self.assertContains(response, 'Login Page')
+
+
+        
 
  
