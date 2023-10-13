127b19a8508db83958c3e7cb6e2e63bac223f02f
saravanan <saravzizo>
2023-10-13 19:16:38 +0530

    only chat balance eod

--- Chat/urls.py
+++ Chat/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import chat_view
 
 urlpatterns = [
-    path('', chat_view, name='chat'),
+    path('<str:username>/', chat_view, name='chat'),
 ]
--- Chat/views.py
+++ Chat/views.py
@@ -3,9 +3,10 @@ from Chat.forms import Add_chat_model
 from Chat.models import Add_chat
 
 
-def chat_view(request):
-    email_ids = Add_chat.objects.values_list('username', flat=True) 
+def chat_view(request,username):
+
+    # email_ids = Add_chat.objects.values_list('username', flat=True) 
     context = {
-        'email_ids': email_ids, 
+        'username': username, 
     }
     return render(request,"chat.html" ,context)
