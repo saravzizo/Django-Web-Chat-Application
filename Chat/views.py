a57fdf86d431d67804b249ef6a4587afb9b420ad
saravanan <saravzizo>
2023-10-20 18:00:23 +0530

    everything is done except float right msg

--- Chat/views.py
+++ Chat/views.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 from django.shortcuts import render
 from Chat.forms import Add_chat_model, Get_message_model
-from Chat.models import Add_chat
+from Chat.models import Add_chat, Messages
 from Chat.consumers import get_common_room_name
 
 
@@ -11,10 +12,17 @@ def chat_view(request,username):
     common_room_name = get_common_room_name(loggedinUser, username)
     print("common_room_name",common_room_name)
     Message_field = Get_message_model(request.POST or None)
+    chat_user = Add_chat.objects.filter(username=username)
+    current_time = datetime.now().time()  
+    chat_history = Messages.objects.filter(room_name=common_room_name).order_by('date')
+
     context = {
         'loggedinUser': loggedinUser,
         'username': username, 
         'common_room_name': common_room_name,
-        'Message_field':Message_field
+        'Message_field':Message_field,
+        'chat_history':chat_history,
+        'chat_user':chat_user,
+        'current_time':current_time,  
     }
     return render(request,"chat.html" ,context)
